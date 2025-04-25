bd1c17391f097e54171513cb7e5e9e2245efdc68
lfiathan <muhammadalifaditya92@gmail.com>
2025-04-25 22:18:51 +0700

    coba menimpa

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,24 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:modisch/core/constants/typography.dart';
-import 'core/constants/colors.dart';
+import 'package:modisch/features/home/presentation/pages/homepage.dart';
+import 'package:modisch/config/theme/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ModischApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ModischApp extends StatelessWidget {
+  const ModischApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.light(
-          primary: AppColors.primary,
-          surface: AppColors.background,
-          secondary: AppColors.secondary,
-          tertiary: AppColors.tertiary,
-        ),
-        textTheme: AppTypography.getM3TextTheme(),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Modisch',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: AppTypography.pageTitle, // Use your page title style
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-              style: AppTypography.buttonLabel, // Use your body text style
-            ),
-            Text(
-              '$_counter',
-              style: AppTypography.pageTitle, // Use appropriate style
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-        backgroundColor: AppColors.tertiary,
-      ),
-    );
-  }
-}
