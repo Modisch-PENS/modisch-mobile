8aacdd535e8af977db02da516374e42c5ccec10e
vantrue <van.true77@gmail.com>
2025-04-26 13:14:37 +0700

    add model and wardrobe card

---
+++ lib/presentation/widgets/model_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../constants/colors.dart';
+
+
+class ModelCard extends StatelessWidget {
+  const ModelCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 0,
+        color: AppColors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: BorderSide(color: Color(0xFFD9D9D9), width: 1.0),
+        ),
+        child: InkWell(
+          splashColor: AppColors.disabled.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: const SizedBox(
+            width: 350,
+            height: 430,
+            child: Text('A card that can be tapped'),
+          ),
+        ),
+      ),
+    );
+  }
+}
