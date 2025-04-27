078c6f9c6f0d905121b1c75e4a466f94a00cfd78
zamzami3 <zalfamumtaza03@gmail.com>
2025-04-27 16:00:01 +0700

    Fitur Make Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:modisch/constants/typography.dart';
-
-import 'constants/colors.dart';
+import 'package:provider/provider.dart';
+import '/model/selected_model.dart';
+import '/screens/preview_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => SelectedModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Make Models',
       theme: ThemeData(
+        primarySwatch: Colors.blue,
         useMaterial3: true,
-        colorScheme: ColorScheme.light(
-          primary: AppColors.primary,
-          surface: AppColors.background,
-          secondary: AppColors.secondary,
-          tertiary: AppColors.tertiary,
-        ),
-        textTheme: AppTypography.getM3TextTheme(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PreviewScreen(),
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -44,11 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -58,22 +55,16 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: AppTypography.pageTitle, // Use your page title style
-        ),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-              style: AppTypography.buttonLabel, // Use your body text style
-            ),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
-              style: AppTypography.pageTitle, // Use appropriate style
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
@@ -82,8 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-        backgroundColor: AppColors.tertiary,
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
