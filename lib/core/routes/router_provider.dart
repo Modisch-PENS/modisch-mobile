be70996b7c0ea0ff7ab7c74aae002adf56cc5a4d
mrrizzz <muhammadrafiriz23@gmail.com>
2025-05-03 17:33:08 +0700

    refactoring onboard page and make it only appears one in lifetime

--- lib/core/routes/router_provider.dart
+++ lib/core/routes/router_provider.dart
@@ -1,18 +1,36 @@
 import 'package:go_router/go_router.dart';
 import 'package:modisch/features/main/pages/main_page.dart';
+import 'package:modisch/features/onboard/riverpod/onboard_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:modisch/features/onboard/page/onboard.dart';
 
 part 'router_provider.g.dart';
 
 @Riverpod(keepAlive: true)
-GoRouter router(RouterRef ref) => GoRouter(
-  routes: [
-    GoRoute(
-      path: '/main',
-      name: 'main',
-      builder: (context, state) => const MainPage(),
-    ),
-  ],
-  initialLocation: '/main',
-  debugLogDiagnostics: false,
-);
+GoRouter router(RouterRef ref) {
+  final hasOnBoarded = ref
+      .watch(onboardStatusProvider)
+      .maybeWhen(data: (value) => value, orElse: () => false);
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        redirect: (context, state) {
+          return hasOnBoarded ? '/main' : '/onboard';
+        },
+      ),
+      GoRoute(
+        path: '/onboard',
+        name: 'onboard',
+        builder: (context, state) => const OnboardPage(),
+      ),
+      GoRoute(
+        path: '/main',
+        name: 'main',
+        builder: (context, state) => const MainPage(),
+      ),
+    ],
+    debugLogDiagnostics: false,
+  );
+}
